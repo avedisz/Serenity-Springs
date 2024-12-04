c9cb2c32281d2ccfa08b7b10e99868d5430d3d5e
avedisz <jianaromero30@gmail.com>
2024-12-04 22:50:31 +0800

    Add files via upload

--- nav.js
+++ nav.js
@@ -8,14 +8,6 @@
             const sidebar = document.querySelector('.sidebar')
             sidebar.style.display= 'none'
          }
-         function showImage(imageSrc, buttonElement) {
-            const imageElement = document.getElementById('displayed-image');
-            const buttons = document.querySelectorAll('.button');
-           
-            imageElement.src = imageSrc;
-            buttons.forEach((button) => {button.classList.remove('focus');});
-            buttonElement.classList.add('focus');
-         }
          function myFunction() {
             window.location.href = "services.html";
          }
